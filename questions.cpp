041b8e6ee2a86cb9cd0c40264aceebed75469ece
Divya Bharti <divyabharti1464@gmail.com>
2022-06-28 19:40:30 +0530

    added fibonnacciSeries

---
+++ questions.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Q1.  int a, b=1;
+    // a=10;
+    // if(++a)
+    //     cout<<b;
+    // else
+    //     cout<< ++b;
+
+
+    // Q 2. int a=1;
+    // int b=2;
+    // if(a-- >0 && ++b >2 )
+    // {
+    //     cout<<"Stage1-inside if ";
+    // }else{
+    //     cout<<"Stage2 -Inside else ";
+    // }
+    // cout<<a<<" "<<b<<endl;
+
+
+    // Q 3. int a=1;
+    //  int b=2;
+    //  if(a-- >0 || ++b >2 )
+    //  {
+    //      cout<<"Stage1-inside if ";
+    //  }else{
+    //      cout<<"Stage2 -Inside else ";
+    //  }
+    //  cout<<a<<" "<<b<<endl;  
+
+
+    // Q 4. int num=3;
+    // cout<<(25*(++num)); 
+
+
+    // Q 5. int a=1;
+    // int b=a++;
+    // int c=++a;
+    // cout<<b;
+    // cout<<c;
+
+
+//    Q 6.  for (int i=0;i<=5;i++)
+//     {
+//         cout<< i<<" ";
+//         i++;
+//     }
+
+    // Q 7. for (int i=0;i<=5;i--)
+    // {
+    //     cout<< i<<" ";
+    //     i++;
+    // }
+
+    // Q 8. for (int i=0;i<=15;i+=2)
+    // {
+    //     cout<<i<<" ";
+    //    // cout<<( i & 1)<<endl;
+    //     if(i&1)
+    //     {
+    //         continue;
+    //     }
+    //     i++;
+    // }
+
+    // Q 9. for(int i=0;i<5;i++)
+    // {
+    //     for(int j=i;j<=5;j++)
+    //     {
+    //         cout<<i<<" "<<j<<endl;
+    //     }
+    // }
+
+
+    //Q. 10   for(int i=0;i<5;i++)
+    // {
+    //     for(int j=i;j<=5;j++)
+    //     {
+    //        if(i+j ==10)
+    //        {
+    //         break;
+    //        }
+    //         cout<<i<<" "<<j<<endl;
+    //     }
+    // }
+
+
+}
